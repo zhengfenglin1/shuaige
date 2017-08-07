bdcc49e98da7bc1a798ef89de87c9d34470be0ac
zhengfenglin1 <zhfl15767701770>
2017-08-07 11:23:05 +0800

    vv

--- src/com/test/proxy/Test.java
+++ src/com/test/proxy/Test.java
@@ -7,7 +7,7 @@ public class Test {
 	public static void main(String[] args) {
 		SunBingBing sbb = new SunBingBing();
 		//经纪人决定让sbb当替身
-		Agent agent = new Agent(sbb);
+		Agent agent = new Agent(sbb);   
 		//得到一个替身
 		FanBingBing fbb = (FanBingBing) Proxy.newProxyInstance(FanBingBing.class.getClassLoader(), new Class[]{FanBingBing.class},agent);
         fbb.jumpFromBuildingTop();
